420280a77b9b4c962b7031276dab8a1d87700182
Divyansh Dixit <114280477+divyanshdixit10@users.noreply.github.com>
2025-05-05 19:56:50 +0530

    Initial commit: Adding project structure and files

---
+++ osop-coding/osop-coding/src/main/java/com/osopcoding/osop_coding/OsopCodingApplication.java
@@ -0,0 +1,13 @@
+package com.osopcoding.osop_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsopCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsopCodingApplication.class, args);
+	}
+
+}
